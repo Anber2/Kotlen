3830a3e29650b72e27b52103436b2f98ce539df5
Anber2 <anber.sub@gmail.com>
2018-02-25 09:21:56 +0200

    //

---
+++ app/src/main/java/com/mawaqaa/kotlen/LogIn.kt
@@ -0,0 +1,28 @@
+package com.mawaqaa.kotlen
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by HP on 2/25/2018.
+ */
+class LogIn : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //EditText !in savedInstanceState = savedInstanceState
+
+
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/mawaqaa/kotlen/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.mawaqaa.kotlen
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //EditText !in savedInstanceState = savedInstanceState
+
+
+
+
+    }
+}
